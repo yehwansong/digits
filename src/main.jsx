e2c8222f3d59587982d87af270bf063c5a0cbb14
yehwansong <yehwanyensong@gmail.com>
2019-09-09 20:21:42 +0900

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/app';
-// import './index.css';
+import './index.css';
 
 ReactDom.render(<App />, document.getElementById('app'));
 ReactDom.render(<App />, document.getElementById('app2'));
