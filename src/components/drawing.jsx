1b80d54581ebd2ce0c09079a3e6eb63958bd1521
yehwansong <yehwanyensong@gmail.com>
2019-09-10 00:16:09 +0900

    add

--- src/components/drawing.jsx
+++ src/components/drawing.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import path from 'path';
 import styled, { injectGlobal } from 'styled-components';
-
 
 import remixreg from './fonts/remix_reg.woff';
 import remixmid from './fonts/39DDDA_0_0.woff';
